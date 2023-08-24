74342d56e5ee0958f4ed641b2f95b4e31bcdf380
blackcater <blackcater2015@gmail.com>
2023-08-25 02:49:25 +0800

    chore: update dependencies

--- alias.ts
+++ alias.ts
@@ -2,4 +2,10 @@ import { resolve } from 'node:path'
 
 const r = (p: string) => resolve(__dirname, p)
 
-export const alias: Record<string, string> = {}
+export const alias: Record<string, string> = {
+  '@krawl/arhive': r('./packages/arhive/src/'),
+  '@krawl/cli': r('./packages/cli/src/'),
+  '@krawl/core': r('./packages/core/src/'),
+  '@krawl/ui': r('./packages/ui/node/'),
+  '@krawl/utils': r('./packages/utils/src/'),
+}
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -5,66 +5,45 @@ import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 import Pages from 'vite-plugin-pages'
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
+import { alias } from '../../alias'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   root: __dirname,
-  base: './',
+  base: command === 'build' ? '/__krawl/' : '/',
   resolve: {
     alias: {
+      ...alias,
       '~/': `${resolve(__dirname, 'client')}`,
     },
   },
-  define: {
-    __BASE_PATH__: '"/__krawl__/"',
-  },
   plugins: [
+    Unocss('uno.config.ts'),
     Vue({
       script: {
         defineModel: true,
         propsDestructure: true,
       },
     }),
-    Unocss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons(),
-      ],
-      shortcuts: {
-        'bg-base': 'bg-white dark:bg-[#111]',
-        'bg-overlay': 'bg-[#eee]:50 dark:bg-[#222]:50',
-        'bg-header': 'bg-gray-500:5',
-        'bg-active': 'bg-gray-500:8',
-        'bg-hover': 'bg-gray-500:20',
-        'border-base': 'border-gray-500:10',
-
-        'tab-button': 'font-light op50 hover:op80 h-full px-4',
-        'tab-button-active': 'op100 bg-gray-500:10',
-      },
-    }),
     Components({
       dirs: ['client/components'],
-      dts: resolve(__dirname, './client/components.d.ts'),
+      dts: 'client/components.d.ts',
     }),
     Pages({
       dirs: ['client/pages'],
     }),
     AutoImport({
-      dts: resolve(__dirname, './client/auto-imports.d.ts'),
-      dirs: [
-        './client/composables',
-      ],
+      dirs: ['client/composables'],
       imports: [
         'vue',
         'vue-router',
         '@vueuse/core',
       ],
+      dts: 'client/auto-imports.d.ts',
       injectAtEnd: true,
     }),
   ],
   build: {
-    outDir: './dist/client',
+    outDir: 'dist/client',
   },
-})
+}))
