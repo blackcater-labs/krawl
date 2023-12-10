a9e61e7639c63f4abeee1d459c729e2ef1ebe664
blackcater <blackcater2015@gmail.com>
2023-12-11 00:56:13 +0800

    feat: add searchbar icon

--- packages/ui/src/app/_components/Header.tsx
+++ packages/ui/src/app/_components/Header.tsx
@@ -1,15 +1,15 @@
+import { SearchBar } from './SearchBar'
 import { LngToggle } from '@/components/LngToggle'
 import { ThemeToggle } from '@/components/ThemeToggle'
 import { Separator } from '@/components/ui/separator'
 
-interface Props {}
-
-export function Header(props: Props) {
+export function Header() {
   return (
     <header className="absolute inset-x-0 top-0  h-[54px] border-b border-border bg-card shadow">
       <div className="container flex h-full items-center justify-between">
         <div>
           {/* Search */}
+          <SearchBar />
         </div>
         <div className="flex items-center space-x-2">
 
--- packages/ui/src/app/layout.tsx
+++ packages/ui/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Atom as IconAtom, Braces as IconBraces, Bug as IconBug, Cable as IconCable, Database as IconDatabase, Loader2 as IconLoader2, Package as IconPackage, Play as IconPlay, Power as IconPower, TerminalSquare as IconTerminalSquare } from 'lucide-react'
+import { AtomIcon, BracesIcon, BugIcon, CableIcon, DatabaseIcon, Loader2Icon, PackageIcon, PlayIcon, PowerIcon, TerminalSquareIcon } from 'lucide-react'
 
 import { Outlet } from '@tanstack/react-router'
 import { Header } from './_components/Header'
@@ -14,52 +14,52 @@ function RootLayout() {
         <main className="container mt-[54px]">
           {/* Play */}
           <Button className="" variant="ghost">
-            <IconPlay className="mr-2 h-4 w-4" />
+            <PlayIcon className="mr-2 h-4 w-4" />
             Start
           </Button>
           {/* Power */}
           <Button className="" variant="ghost">
-            <IconPower className="mr-2 h-4 w-4" />
+            <PowerIcon className="mr-2 h-4 w-4" />
             Stop
           </Button>
           {/* Loader */}
           <Button className="" variant="ghost">
-            <IconLoader2 className="mr-2 h-4 w-4 animate-spin" />
+            <Loader2Icon className="mr-2 h-4 w-4 animate-spin" />
             Loader
           </Button>
           {/* Storage */}
           <Button className="" variant="ghost">
-            <IconDatabase className="mr-2 h-4 w-4" />
+            <DatabaseIcon className="mr-2 h-4 w-4" />
             Storage
           </Button>
           {/* Config */}
           <Button className="" variant="ghost">
-            <IconBraces className="mr-2 h-4 w-4" />
+            <BracesIcon className="mr-2 h-4 w-4" />
             Config
           </Button>
           {/* Plugins */}
           <Button className="" variant="ghost">
-            <IconPackage className="mr-2 h-4 w-4" />
+            <PackageIcon className="mr-2 h-4 w-4" />
             Plugins
           </Button>
           {/* Graph */}
           <Button className="" variant="ghost">
-            <IconAtom className="mr-2 h-4 w-4" />
+            <AtomIcon className="mr-2 h-4 w-4" />
             Graph
           </Button>
           {/* Log */}
           <Button className="" variant="ghost">
-            <IconTerminalSquare className="mr-2 h-4 w-4" />
+            <TerminalSquareIcon className="mr-2 h-4 w-4" />
             Log
           </Button>
           {/* Audit */}
           <Button className="" variant="ghost">
-            <IconBug className="mr-2 h-4 w-4" />
+            <BugIcon className="mr-2 h-4 w-4" />
             Audit
           </Button>
           {/* Progess */}
           <Button className="" variant="ghost">
-            <IconCable className="mr-2 h-4 w-4" />
+            <CableIcon className="mr-2 h-4 w-4" />
             Progress
           </Button>
           <Outlet />
